0f522af2c0d24116c7af2b0916fb89deeb33807c
Zuhaib Sheikh Mahmood <mahmoo21@msu.edu>
2016-11-07 14:06:04 -0500

    added sorting functionality to dict2csv() in zmcsvfunctions

--- zmcsvfunctions.py
+++ zmcsvfunctions.py
@@ -1,5 +1,5 @@
 #! usr/bin/env python
-import os,csv
+import os,csv,sys
 
 def writerow(row,ofile,odir,aw):
 	curdir=os.getcwd()
@@ -9,14 +9,12 @@ def writerow(row,ofile,odir,aw):
 		writer.writerow(row)
 	os.chdir(curdir)
 
-
 def normalize_row(row,lenorig):
 	if len(row)<lenorig:
 		diff=lenorig-len(row)
 		for i in range(0,diff):
 			row.append('')
 	return row
-
 
 def concat_csvs(csvfile_list,indir,csvoutname):
     if len(csvfile_list)>1:
@@ -40,24 +38,24 @@ def concat_csvs(csvfile_list,indir,csvoutname):
     elif len(csvfile_list)==1:
         return csvfile_list[0]
 
-def dict2csv(indict,ofile,odir,keyname='Key',valuename='Value'):
+def dict2csv(indict,ofile,odir,keyname='Key',valuename='Value',out_sort_key=False):
     #Maximum depth of keys and values is 1-dimension. For example,
     #(2011,482,2):[200,600] is written as row [2011,482,2,200,600].
     #If there's more depth, you'll just get that list as string in your row.
     #Note that lists and tuples are treated the same, in that
     #elements are written out itemwise.
     if type(keyname)==str:
-    	_keyhead=[keyname]
+        _keyhead=[keyname]
     elif type(keyname)==list:
-    	_keyhead=keyname
+        _keyhead=keyname
     else:
-    	sys.exit('for dict2csv, please pass a string or list for your key/val names')
+        sys.exit('for dict2csv, please pass a string or list for your key/val names')
     if type(valuename)==str:
-    	_valuehead=[valuename]
+        _valuehead=[valuename]
     elif type(valuename)==list:
-    	_valuehead=valuename
+        _valuehead=valuename
     else:
-    	sys.exit('for dict2csv, please pass a string or list for your key/val names')
+        sys.exit('for dict2csv, please pass a string or list for your key/val names')
 
     _header=_keyhead+_valuehead
     curdir=os.getcwd()
@@ -66,20 +64,45 @@ def dict2csv(indict,ofile,odir,keyname='Key',valuename='Value'):
         writer=csv.writer(output)
         writer.writerow(_header)
     
-    for k,v in indict.items():
-        _a=[]
-        if type(k)==list or type(k)==tuple:
-        	for _i in k:
-        		_a.append(_i)
-       	else:
-       		_a.append(k)
-        if type(v)==list or type(v)==tuple:
-            for _i in v:
-                _a.append(_i)
-        else:
-        	_a.append(v)
- 
-        with open(ofile,'a') as output2:
-            writer=csv.writer(output2)
-            writer.writerow(_a)
-    os.chdir(curdir)
+    if out_sort_key==True and type(keyname)==list:
+        sys.exit("dict2csv sorting functionality only available for single-key dictionaries")
+    
+    if out_sort_key==False:
+        for k,v in indict.items():
+            _a=[]
+            if type(k)==list or type(k)==tuple:
+                for _i in k:
+                   _a.append(_i)
+            else:
+                _a.append(k)
+            if type(v)==list or type(v)==tuple:
+                for _i in v:
+                    _a.append(_i)
+            else:
+                _a.append(v)
+
+            with open(ofile,'a') as output2:
+                writer=csv.writer(output2)
+                writer.writerow(_a)
+
+    elif out_sort_key==True:
+        _araw=[]
+        for k,v in indict.items():
+            _tuplist=[]
+            if type(k)==list or type(k)==tuple:
+                sys.exit("dict2csv sorting functionality only available for single-key dictionaries")
+            _tuplist.append(k)
+            if type(v)==list or type(k)==tuple:
+                for _i in v:
+                    _tuplist.append(_i)
+            else:
+                _tuplist.append(v)
+
+            _tup=tuple(_tuplist)
+            _araw.append(_tup)
+        _a=sorted(_araw,key=lambda x:x[0])
+        for _tup in _a:
+            _fin=list(_tup)
+            with open(ofile,'a') as output2:
+                writer=csv.writer(output2)
+                writer.writerow(_fin)
